ac9011d3c67c5336184689193b073fcab1db2fc4
Zahz <martineggemark@gmail.com>
2024-07-12 12:10:38 +0200

    Update solver.py
    Removed stuff that is already in the README.md and added line in the beginning to tell the OS that it is a python file.

--- solver.py
+++ solver.py
@@ -1,31 +1,4 @@
-'''
-
-Got a clip in my recommended of Henya the Genious playing Silent Hill 3, and actually being a real genious.
-So before watching the end, I wanted to write a program to find the solution.
-
-The normal puzzle gives these two hints:
-
-    "The first is larger than the second;
-    the second twice the third;
-    the third smaller than the fourth;
-    the fourth is half the first.
-
-
-    "The first is larger than the second;
-    the second twice the third;
-    the third smaller than the fourth;
-    the fourth is half the first.
-
-Keypad looks like this, and is not a "normal" modern keypad with a 0 and two extra function buttons. It only contains the number 1 - 9.
-
-    1 2 3
-    4 5 6
-    7 8 9
-
-
-https://silenthill.fandom.com/wiki/Brookhaven_Keypad#Normal
-
-'''
+#!/usr/bin/env python3
 
 def solver():
     list_of_sequences = ['No_Found']
